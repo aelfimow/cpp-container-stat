92ed853a8b658dbd6572fb296ed1c550fa824354
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-07 20:54:23 +0100

    Create new container

--- src/CppContainer.cpp
+++ src/CppContainer.cpp
@@ -7,6 +7,7 @@
 #include "vector_uint8.h"
 #include "vector_uint16.h"
 #include "vector_uint32.h"
+#include "vector_uint64.h"
 
 
 IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
@@ -15,7 +16,8 @@ IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
     {
         { "vector_uint8",  [&]() { return new vector_uint8 { cycles }; } },
         { "vector_uint16", [&]() { return new vector_uint16 { cycles }; } },
-        { "vector_uint32", [&]() { return new vector_uint32 { cycles }; } }
+        { "vector_uint32", [&]() { return new vector_uint32 { cycles }; } },
+        { "vector_uint64", [&]() { return new vector_uint64 { cycles }; } },
     };
 
     if (auto it = containers.find(type); it != containers.end())
