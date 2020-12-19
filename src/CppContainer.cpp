8c78f2e925ffd254f069dcdf2c60ddb7148ed353
Alexander Elfimow <my.free.mail@gmx.de>
2020-12-19 22:25:54 +0100

    Added new container

--- src/CppContainer.cpp
+++ src/CppContainer.cpp
@@ -10,6 +10,7 @@
 #include "vector_uint64.h"
 #include "list_uint8.h"
 #include "list_uint16.h"
+#include "list_uint32.h"
 
 
 IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
@@ -21,7 +22,8 @@ IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
         { "vector_uint32", [&]() { return new vector_uint32 { cycles }; } },
         { "vector_uint64", [&]() { return new vector_uint64 { cycles }; } },
         { "list_uint8",    [&]() { return new list_uint8 { cycles }; } },
-        { "list_uint16",   [&]() { return new list_uint16 { cycles }; } }
+        { "list_uint16",   [&]() { return new list_uint16 { cycles }; } },
+        { "list_uint32",   [&]() { return new list_uint32 { cycles }; } }
     };
 
     auto it = containers.find(type);
