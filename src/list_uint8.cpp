f546b4b187e1ecff6bb857e3a55529c8dc600153
Alexander Elfimow <my.free.mail@gmx.de>
2021-03-07 20:48:05 +0100

    list_uint8: push back value

--- src/list_uint8.cpp
+++ src/list_uint8.cpp
@@ -12,6 +12,7 @@ void list_uint8::run()
 {
     for (size_t i = 0; i < m_cycles; ++i)
     {
-        m_buffer.push_back(0);
+        uint8_t value = static_cast<uint8_t>(i);
+        m_buffer.push_back(value);
     }
 }
