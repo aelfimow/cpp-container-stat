ab07f7760bb1bb59ea2c09819f61558330e16cb5
Alexander Elfimow <my.free.mail@gmx.de>
2021-03-07 20:51:54 +0100

    forward_list_uint32: push front value

--- src/forward_list_uint32.cpp
+++ src/forward_list_uint32.cpp
@@ -12,6 +12,7 @@ void forward_list_uint32::run()
 {
     for (size_t i = 0; i < m_cycles; ++i)
     {
-        m_buffer.push_front(0);
+        uint32_t value = static_cast<uint32_t>(i);
+        m_buffer.push_front(value);
     }
 }
