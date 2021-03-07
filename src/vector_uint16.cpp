d539fbac5a8068f14432891f0a2bb6e26c197da8
Alexander Elfimow <my.free.mail@gmx.de>
2021-03-07 20:45:29 +0100

    vector_uint16: push back value

--- src/vector_uint16.cpp
+++ src/vector_uint16.cpp
@@ -12,6 +12,7 @@ void vector_uint16::run()
 {
     for (size_t i = 0; i < m_cycles; ++i)
     {
-        m_buffer.push_back(0);
+        uint16_t value = static_cast<uint16_t>(i);
+        m_buffer.push_back(value);
     }
 }
