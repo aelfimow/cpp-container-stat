dac0bb6cdfd1121f18e69dc558ce9f26320c3940
Alexander Elfimow <my.free.mail@gmx.de>
2021-05-09 11:54:47 +0200

    Use new class

--- src/CppContainer.cpp
+++ src/CppContainer.cpp
@@ -5,6 +5,7 @@
 #include "CppContainer.h"
 #include "IContainer.h"
 #include "empty.h"
+#include "map_uint8.h"
 #include "vector_uint8.h"
 #include "vector_uint16.h"
 #include "vector_uint32.h"
@@ -27,6 +28,7 @@ IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
     std::map<std::string, std::function<IContainer *()>> const containers
     {
         { "empty",               [&]() { return new empty { cycles }; } },
+        { "map_uint8",           [&]() { return new map_uint8 { cycles }; } },
         { "vector_uint8",        [&]() { return new vector_uint8 { cycles }; } },
         { "vector_uint16",       [&]() { return new vector_uint16 { cycles }; } },
         { "vector_uint32",       [&]() { return new vector_uint32 { cycles }; } },
