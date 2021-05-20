fb9f6dd20142fed00496e29757b9b1c2330721d1
Alexander Elfimow <my.free.mail@gmx.de>
2021-05-20 21:26:29 +0200

    Use new class

--- src/CppContainer.cpp
+++ src/CppContainer.cpp
@@ -50,6 +50,7 @@
 #include "forward_list_long_double.h"
 #include "forward_list_float128.h"
 #include "forward_list_complex128.h"
+#include "deque_uint8.h"
 
 
 IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
@@ -100,7 +101,8 @@ IContainer *CppContainer::instantiate(std::string const &type, size_t cycles)
         { "forward_list_double",        [&]() { return new forward_list_double { cycles }; } },
         { "forward_list_long_double",   [&]() { return new forward_list_long_double { cycles }; } },
         { "forward_list_float128",      [&]() { return new forward_list_float128 { cycles }; } },
-        { "forward_list_complex128",    [&]() { return new forward_list_complex128 { cycles }; } }
+        { "forward_list_complex128",    [&]() { return new forward_list_complex128 { cycles }; } },
+        { "deque_uint8",                [&]() { return new deque_uint8 { cycles }; } }
     };
 
     auto it = containers.find(type);
